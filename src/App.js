471de2912654bd215d52b471fb5ced41f403d7f6
MacBook Pro <macbookpro@MacBook-Pro-Macbook.local>
2022-11-26 09:37:44 -0800

    finished project

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import Timer from "./components/timer/Timer";
-import './index.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="container">
-      <Timer />
-    </div>
-  );
-}
+import { AboutUs, Chef, FindUs, Footer, Gallery, Header, Intro, Laurels, SpecialMenu } from './container';
+import { Navbar } from './components';
+import './App.css';
+
+const App = () => (
+  <div>
+    <Navbar />
+    <Header />
+    <AboutUs />
+    <SpecialMenu />
+    <Chef />
+    <Intro />
+    <Laurels />
+    <Gallery />
+    <FindUs />
+    <Footer />
+  </div>
+);
 
 export default App;
